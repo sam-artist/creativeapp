9a032c2eff7f68888274daa24fdf39c9744be78e
sam-artist <siw25@cornell.edu>
2021-05-25 15:44:51 -0400

    site updates

--- submissions.php
+++ submissions.php
@@ -12,7 +12,8 @@ $fullName = $firstName . " " . $lastName;
 
 function text_print($firstName, $lastName, $comment){
   $fullName=$firstName." ". $lastName;
-  echo "Thanks you ".htmlspecialchars($fullName)." for visting our page!"." Your comment-- ". htmlspecialchars($comment)."--was great!";
+  echo "Thanks you ".htmlspecialchars($fullName)." for visiting my blog. You comments and feedback are greatly appreciated. I will do my best
+  to respond to any inquiries left for me. Thanks again - Elizabeth";
   }
 ?>
 
@@ -25,11 +26,16 @@ function text_print($firstName, $lastName, $comment){
   <meta name="viewport" content="width=device-width, initial-scale=1" />
 
   <title>submission</title>
-  <link rel="stylesheet" type="text/css" href="styles/all.css" media="all" />
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Great+Vibes">
+    <link rel="stylesheet" type="text/css" href="styles/all.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="styles/tablet.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="styles/desktop.css" media="all" />
+  
 </head>
 
 <body>
-  <?php include("includes/header.php"); ?>
+  <?php include("includes/navigation.php"); ?>
 
 
   <div class="submit">
@@ -37,6 +43,15 @@ function text_print($firstName, $lastName, $comment){
       <?php echo text_print($firstName, $lastName, $comment); ?>
     </p>
   </div>
-
+  <footer>
+      <p>
+      <span class="bold" >Contact:
+        creativeapp@gmail.com
+        <br>
+      </span>
+     
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat Duis aute irure.
+      </p>
+  </footer>
 </body>
 </html>
